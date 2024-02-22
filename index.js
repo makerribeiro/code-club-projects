48baaf19103d9b02ee7588d0fa74852632acdc35
makerribeiro <paulorodribeiro@gmail.com>
2024-02-22 19:52:27 -0300

    First-project-React

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ app.use(express.json())
 const users = []
 
 const checkUserId = (request, response, next) => {
-    const { id} = request.params
+    const { id } = request.params
     const index = users.findIndex(user => user.id === id)
 
-        if(index < 0){
-            return response.status(404).json({message: "User not found"})
+    if (index < 0) {
+        return response.status(404).json({ message: "User not found" })
     }
 
     request.userIndex = index
@@ -23,41 +23,41 @@ const checkUserId = (request, response, next) => {
 
 app.get('/users', (request, response) => {
 
-return response.json(users)
+    return response.json(users)
 
 })
 app.post('/users', (request, response) => {
-    const { name, age} = request.body
+    const { name, age } = request.body
 
-    const user = { id:uuid.v4(), name, age}
+    const user = { id: uuid.v4(), name, age }
 
     users.push(user)
-    
+
     return response.status(201).json(user)
-    
-    })
- app.put('/users/:id', checkUserId, (request, response) => {
-        const { name, age} = request.body
-        const index = request.userIndex
-        const id = request.userId
 
-        const updateUser = { id, name, age}
+})
+app.put('/users/:id', checkUserId, (request, response) => {
+    const { name, age } = request.body
+    const index = request.userIndex
+    const id = request.userId
 
-        users[index] = updatedUser
+    const updateUser = { id, name, age }
 
-        return response.json(updatedUser)
-        
-        })
+    users[index] = updatedUser
+
+    return response.json(updatedUser)
+
+})
 
 app.delete('/users/:id', checkUserId, (request, response) => {
-            const index = request.userIndex
+    const index = request.userIndex
 
-             users.splice(index,1)
+    users.splice(index, 1)
 
-            return response.status(204).json()
-            
-            })
+    return response.status(204).json()
 
-app.listen(port, () =>{
+})
+
+app.listen(port, () => {
     console.log('Server started on port $(port)')
 })
