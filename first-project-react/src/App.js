4ad90d295b0c63cb9ab646434ed26d817c834cd5
makerribeiro <paulorodribeiro@gmail.com>
2024-02-27 06:41:06 -0300

    Alterações no projeto 1

--- first-project-react/src/App.js
+++ first-project-react/src/App.js
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
-import { Container, H1, Image, ContainerItems, InputLabel, Input, Button } from "./styles";
+import People from './assets/people.svg';
+import Arrow from './assets/arrow.svg';
+import Trash from './assets/trash.svg';
+
+import { Container, H1, Image, ContainerItems, InputLabel, Input, Button, User } from "./styles";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const inputName = useRef()
+  const inputAge = useRef()
+
+  function addNewUser() {
+    setUsers([...users, { id: Math.random(), name: inputName.current.value, age: inputAge.current.value }]);
+
+  }
+
+  function deleteUser(userId) {
+    const newUsers = users.filter((user) => user.id !== userId );
+    setUsers(newUsers);
+  }
+
 
   return (
     <Container>
-      <Image/>
+      <Image alt="logo-imagem" src={People} />
       <ContainerItems>
-      <H1>Olá</H1>
-      <InputLabel>Nome</InputLabel>
-      <Input placeholder="Nome"/>
+        <H1>Olá</H1>
+        <InputLabel>Nome</InputLabel>
+        <Input ref={inputName} placeholder="Nome" />
 
-      <InputLabel>Idade</InputLabel>
-      <Input placeholder="Idade"/>
+        <InputLabel>Idade</InputLabel>
+        <Input ref={inputAge} placeholder="Idade" />
 
-      <Button>Cadastrar</Button>
+        <Button onClick={addNewUser}>
+          Cadastrar<img alt="seta" src={Arrow} />
+        </Button>
+
+        <ul>
+          {users.map(user => (
+            <User key={user.id}>
+              <p>{user.name}</p> <p>{user.age}</p>
+              <button onClick={() => deleteUser(user.id)}>
+                <img src={Trash} alt="lata-de-lixo" />
+                </button>
+            </User>
+          ))
+          }
+        </ul>
 
       </ContainerItems>
     </Container>
